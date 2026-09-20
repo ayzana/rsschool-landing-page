ec2dbd1ed6bce237bc403a8f5bc1b06a5ccf6276
Ayzanat Gabieva <aizanka001@gmail.com>
2026-09-20 09:35:17 +0300

    fix: correct switching theme js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 "use strict";
 
-const themeBTN = document.querySelector(".theme");
+const themeDarkBTN = document.querySelector(".theme-dark");
+const themeLightBTN = document.querySelector(".theme-light");
 
-themeBTN.addEventListener("click", function () {
+themeDarkBTN.addEventListener("click", function () {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   let newTheme = "light";
   if (currentTheme === "light") newTheme = "dark";
   document.documentElement.setAttribute("data-theme", newTheme);
+  themeDarkBTN.classList.add("active");
+  themeLightBTN.classList.remove("active");
+  localStorage.setItem("theme", currentTheme);
+});
+
+themeLightBTN.addEventListener("click", function () {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  let newTheme = "light";
+  if (currentTheme === "light") newTheme = "dark";
+  document.documentElement.setAttribute("data-theme", newTheme);
+  themeLightBTN.classList.add("active");
+  themeDarkBTN.classList.remove("active");
   localStorage.setItem("theme", currentTheme);
 });
