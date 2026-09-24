5d205ed61c7da5578a4b2a5b6942c7b3337bef58
Ayzanat Gabieva <aizanka001@gmail.com>
2026-09-24 22:56:36 +0300

    feat: add opening burger menu function

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const themeDarkBTN = document.querySelector(".theme-dark");
 const themeLightBTN = document.querySelector(".theme-light");
 
 const currentTheme = localStorage.getItem("theme");
-if (currentTheme){
-document.documentElement.setAttribute("data-theme", currentTheme);
-   if (currentTheme === "light"){
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === "light") {
     themeDarkBTN.classList.remove("active");
     themeLightBTN.classList.add("active");
-   } else {
+  } else {
     themeDarkBTN.classList.add("active");
     themeLightBTN.classList.remove("active");
-   }
   }
-  
+}
+
 themeDarkBTN.addEventListener("click", function () {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   let newTheme = "light";
@@ -33,4 +33,26 @@ themeLightBTN.addEventListener("click", function () {
   themeLightBTN.classList.add("active");
   themeDarkBTN.classList.remove("active");
   localStorage.setItem("theme", newTheme);
+});
+
+/* Burger */
+const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burger-menu");
+const burgerItems = document.querySelectorAll(".burger-item");
+
+function toggleBurgerMenu() {
+  burger.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
+
+  if (burger.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
+}
+
+burger.addEventListener("click", toggleBurgerMenu);
+
+burgerItems.forEach(function (item) {
+  item.addEventListener("click", toggleBurgerMenu);
 });
