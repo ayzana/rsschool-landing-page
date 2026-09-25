86f070ba2007f2140273617d50c55a6d3bda1c2b
Ayzanat Gabieva <aizanka001@gmail.com>
2026-09-25 23:39:10 +0300

    feat: add create menu item function

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
 "use strict";
+import data from "./data/products.json" with { type: "json" };
+
+const item = data[0];
+
+function createMenuItem(item) {
+  const el = document.createElement("li");
+  el.className = "menu-item";
+  el.dataset.category = item.category;
+
+  const imageBox = document.createElement("div");
+  imageBox.className = "menu-image-box";
+
+  const menuImage = document.createElement("img");
+  menuImage.className = "menu-image";
+  menuImage.src = `./assets/images/${item.category}-1.png`;
+  menuImage.alt = item.category;
+  imageBox.append(menuImage);
+
+  const itemDescr = document.createElement("div");
+  itemDescr.className = "menu-item-descr";
+
+  const descrText = document.createElement("div");
+  descrText.className = "descr-text";
+  const title = document.createElement("p");
+  title.className = "descr-title";
+  title.textContent = item.name;
+  const descrInfo = document.createElement("p");
+  descrInfo.className = "descr-info";
+  descrInfo.textContent = item.description;
+  descrText.append(title, descrInfo);
+
+  const price = document.createElement("p");
+  price.className = "price";
+  price.textContent = "$" + item.price;
+  itemDescr.append(descrText, price);
+
+  el.append(imageBox, itemDescr);
+  return el;
+}
+document.querySelector(".menu-list").appendChild(createMenuItem(item));
+/* Theme */
 
 const themeDarkBTN = document.querySelector(".theme-dark");
 const themeLightBTN = document.querySelector(".theme-light");
