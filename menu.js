0ee582018519d38a9157d855ad852a6f2d102d20
Ayzanat Gabieva <aizanka001@gmail.com>
2026-09-26 16:54:35 +0300

    feat: add show modal

--- menu.js
+++ menu.js
@@ -3,13 +3,14 @@ import data from "./data/products.json" with { type: "json" };
 
 window.onload = function () {
   tabsClickHandler();
+  menuItemsClickHandler();
 };
 
 function createMenuItem(item) {
   const el = document.createElement("li");
   el.className = "menu-item";
   el.dataset.category = item.category;
-
+  el.dataset.id = item.id;
   const imageBox = document.createElement("div");
   imageBox.className = "menu-image-box";
 
@@ -74,5 +75,71 @@ const filterMenuItems = (activeTab) => {
 
     if (item.dataset.category == activeTab.trim())
       item.classList.remove("hidden");
+  });
+};
+
+/*Modal*/
+const modalWrapper = document.querySelector(".modal-overlay");
+const modalContainer = document.querySelector(".modal-wrapper");
+
+modalContainer.addEventListener("click", (event) => {
+  event._isClickWithInMenu = true;
+});
+
+const modalClose = () => {
+  modalWrapper.classList.add("hidden");
+  document.body.style.overflow = "auto";
+};
+
+modalWrapper.addEventListener("click", (event) => {
+  if (event._isClickWithInMenu) return;
+  modalClose();
+});
+const createModal = (item) => {
+  const el = document.createElement("div");
+  el.className = "modal";
+
+  const imageBox = document.createElement("div");
+  imageBox.className = "menu-image-box";
+
+  const menuImage = document.createElement("img");
+  menuImage.className = "menu-image";
+  menuImage.src = `./assets/images/${item.id}.png`;
+  menuImage.alt = item.category;
+  imageBox.append(menuImage);
+
+  const itemDescr = document.createElement("div");
+  itemDescr.className = "menu-item-descr";
+
+  const descrText = document.createElement("div");
+  descrText.className = "descr-text";
+  const title = document.createElement("p");
+  title.className = "descr-title";
+  title.textContent = item.name;
+  const descrInfo = document.createElement("p");
+  descrInfo.className = "descr-info";
+  descrInfo.textContent = item.description;
+  descrText.append(title, descrInfo);
+
+  const price = document.createElement("p");
+  price.className = "price";
+  price.textContent = "$" + item.price;
+  itemDescr.append(descrText, price);
+
+  el.append(imageBox, itemDescr);
+  return el;
+};
+const modalShow = (item) => {
+  modalContainer.append(createModal(item));
+  modalWrapper.classList.remove("hidden");
+  document.body.style.overflow = "hidden";
+};
+const menuItemsClickHandler = () => {
+  document.querySelector(".menu-list").addEventListener("click", (e) => {
+    if (e.target.closest(".menu-item")) {
+      let activeItem = e.target.closest(".menu-item");
+      console.log(activeItem.dataset.id);
+      modalShow(activeItem);
+    }
   });
 };
